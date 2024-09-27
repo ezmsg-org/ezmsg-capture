014f963ed781498d7c9d1c4808fdca3decac7b95
Preston Peranich <pperanich@gmail.com>
2024-09-27 17:58:38 -0400

    fix: import version from generated file.

--- src/ezmsg/capture/__init__.py
+++ src/ezmsg/capture/__init__.py
@@ -4,7 +4,7 @@ import ezmsg.core as ez
 from ezmsg.util.messages.axisarray import AxisArray
 from typing import Optional
 
-__version__ = "0.1.0"
+from .__version__ import __version__ as __version__
 
 
 class VideoCaptureSettings(ez.Settings):
